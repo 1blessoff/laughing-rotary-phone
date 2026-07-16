f135a56687a5bfb2ec1718a39ccabf1062233e96
1blessoff <koukachrist48@gmail.com>
2026-07-16 22:36:35 +0100

    sarah est passer par la lol

--- backend/core/wsgi.py
+++ backend/core/wsgi.py
@@ -15,4 +15,5 @@ from django.core.wsgi import get_wsgi_application
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
 application = get_wsgi_application()
